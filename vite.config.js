fab21ac9061789e272fe451f81bffedf937f4396
kiran yadav <kiranyadav161196@gmail.com>
2024-02-08 18:10:57 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd());
 
     return {
+        base:"/propose",
         plugins: [
             react(),
             createHtmlPlugin({
